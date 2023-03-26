46ca1655951a43ea94323ad9dfbee91c2b0d66fe
rootEnginear <rootEnginear@users.noreply.github.com>
2023-03-27 04:05:04 +0700

    chore: cleanup react integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikReact } from "@builder.io/qwik-react/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { qwikReact } from "@builder.io/qwik-react/vite";
 
 export default defineConfig(() => {
   return {
