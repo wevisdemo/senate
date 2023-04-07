ed92537ab3d39012e56dc730713110507c2428da
rootEnginear <rootEnginear@users.noreply.github.com>
2023-04-07 21:06:18 +0700

    feat: add letter animation

--- src/components/chapter3/Ch3Letter.tsx
+++ src/components/chapter3/Ch3Letter.tsx
@@ -1,6 +1,7 @@
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import QLetter from "../react/QLetter";
 import Lottie from "../Lottie";
+import { animate, inView } from "motion";
 
 export interface Ch3LetterProps {
   title: string;
@@ -8,8 +9,37 @@ export interface Ch3LetterProps {
 }
 
 const Ch3Letter = component$<Ch3LetterProps>(({ title, lottieName }) => {
+  const elLetter = useSignal<HTMLDivElement>();
+
+  useVisibleTask$(
+    () => {
+      if (elLetter.value) {
+        elLetter.value.classList.add("opacity-0");
+
+        inView(
+          elLetter.value,
+          () => {
+            if (elLetter.value)
+              animate(
+                elLetter.value,
+                {
+                  transform: ["rotate(-2deg) scale(1.1)", "rotate(0deg) scale(1)"],
+                  opacity: [0, 1],
+                },
+                { duration: 0.5 }
+              );
+          },
+          {
+            amount: 0.5,
+          }
+        );
+      }
+    },
+    { strategy: "document-idle" }
+  );
+
   return (
-    <div class="w-full overflow-hidden">
+    <div ref={elLetter} class="w-full origin-top overflow-hidden">
       <QLetter title={title}>
         <Slot />
       </QLetter>
