c8b64e563fdb96d8c843ac5d8797d7e55c2bf071
rootEnginear <rootEnginear@users.noreply.github.com>
2023-05-16 13:04:45 +0700

    fix: typos

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -2,9 +2,12 @@ import { component$ } from "@builder.io/qwik";
 
 import { QAboutText } from "~/components/react/QAboutText";
 import { QWvSharer } from "~/components/react/QWvSharer";
+import { QAboutButtons } from "~/components/react/QAboutButtons";
 
 export default component$(() => (
   <QAboutText>
+    <QAboutButtons />
+    <div class="mb-[3rem]" />
     <QWvSharer center />
   </QAboutText>
 ));
